477494a25a4f7c1a0f22670981017f847c3e8fca
Angkarn Pukawanach <angkarn.pk@gmail.com>
2024-04-17 13:00:39 +0700

    Remove the commented-out code.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,21 +1,4 @@
-pub mod generate;
 pub mod console;
+pub mod generate;
 pub mod process_file;
 pub mod template;
-
-// use csv::Error as ErrorCsv;
-
-// // Get data from csv format file
-// pub fn process_file(path: &String) -> Result<Vec<Vec<String>>, ErrorCsv> {
-//     let mut reader = csv::ReaderBuilder::new()
-//         .has_headers(false)
-//         .from_path(path)?;
-
-//     let mut csv_data: Vec<Vec<String>> = Vec::new();
-
-//     for record in reader.records() {
-//         csv_data.push(record.unwrap().iter().map(String::from).collect());
-//     }
-
-//     Ok(csv_data)
-// }
