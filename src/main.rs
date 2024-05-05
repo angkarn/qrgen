ba99b9d60250cf9d51ac7fac26402d094ccaed4a
Angkarn Pukawanach <angkarn.pk@gmail.com>
2024-05-06 02:06:30 +0700

    feat: add base64 format output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod utils;
+use base64::{engine::general_purpose, Engine};
 use clap::{Parser, Subcommand};
+use imageproc::image::ImageFormat;
 use rayon::prelude::*;
-use std::fs::{create_dir_all, metadata};
+use std::{
+    fs::{create_dir_all, metadata},
+    io::Cursor,
+};
 
 /// QR Code Generator Tools
 #[derive(Parser, Debug)]
@@ -22,7 +27,7 @@ enum Command {
 
 #[derive(Parser, Debug)]
 struct CommonArg {
-    /// Format output (console|png) "console" will no custom text
+    /// Format output (console|png|base64)
     #[clap(short = 'f', long, default_value = "console")]
     format: String,
 
@@ -155,7 +160,30 @@ fn handle_gen_command(gen_opt: &GenArg) {
             match handler_result_generate_image(
                 result,
                 format!("{}/{}.png", gen_opt.common_arg.outdir, "qr"),
+                false,
             ) {
+                Ok(r) => println!("{}", r),
+                Err(e) => println!("{}", e),
+            }
+        }
+        "base64" => {
+            let result = utils::generate::generate_image(
+                gen_opt.content.clone(),
+                gen_opt.common_arg.size,
+                &gen_opt.top_text,
+                &gen_opt.bottom_text,
+                gen_opt.common_arg.top_space,
+                gen_opt.common_arg.bottom_space,
+                &gen_opt.common_arg.top_text_pos,
+                &gen_opt.common_arg.bottom_text_pos,
+                &gen_opt.common_arg.font_path,
+                gen_opt.common_arg.font_size,
+                gen_opt.common_arg.no_reduce_text_size,
+                gen_opt.common_arg.add_text_line_space,
+                &gen_opt.common_arg.error_correction_level,
+            );
+
+            match handler_result_generate_image(result, "".to_owned(), true) {
                 Ok(r) => println!("{}", r),
                 Err(e) => println!("{}", e),
             }
@@ -172,7 +200,8 @@ fn handle_from_command(from_opt: &FromArg) {
     match from_opt.common_arg.format.as_str() {
         "console" => generate_list_console(list_data, from_opt),
 
-        "png" => generate_list_image(list_data, from_opt),
+        "png" => generate_list_image(list_data, from_opt, false),
+        "base64" => generate_list_image(list_data, from_opt, true),
         _ => {
             eprintln!("Can't found this format!")
         }
@@ -187,7 +216,7 @@ fn generate_list_console(list_data: Vec<Vec<String>>, from_opt: &FromArg) {
     return;
 }
 
-fn generate_list_image(list_data: Vec<Vec<String>>, from_opt: &FromArg) {
+fn generate_list_image(list_data: Vec<Vec<String>>, from_opt: &FromArg, to_base64: bool) {
     let _ = create_dir_all(from_opt.common_arg.outdir.to_string())
         .expect("Cannot create output directory!");
 
@@ -230,7 +259,7 @@ fn generate_list_image(list_data: Vec<Vec<String>>, from_opt: &FromArg) {
                 &from_opt.common_arg.error_correction_level,
             );
 
-            match handler_result_generate_image(result, path_output_file) {
+            match handler_result_generate_image(result, path_output_file, to_base64) {
                 Ok(r) => {
                     println!("{}", r);
                     true
@@ -252,6 +281,7 @@ fn generate_list_image(list_data: Vec<Vec<String>>, from_opt: &FromArg) {
 fn handler_result_generate_image(
     result: Result<utils::generate::ResultGenerateImage, String>,
     path: String,
+    to_base64: bool,
 ) -> Result<String, String> {
     match result {
         Ok(r) => {
@@ -273,11 +303,21 @@ fn handler_result_generate_image(
                 );
             }
 
-            let save_image = r.image_buffer.into_luma8().save(&path);
+            if to_base64 {
+                let mut bytes: Vec<u8> = Vec::new();
+                r.image_buffer
+                    .write_to(&mut Cursor::new(&mut bytes), ImageFormat::Png)
+                    .expect("Couldn't write image to bytes.");
 
-            match save_image {
-                Ok(_) => Ok(format!("Created: {}", &path)),
-                Err(e) => Err(e.to_string()),
+                let b64 = general_purpose::STANDARD.encode(bytes);
+                Ok(format!("{}", &b64))
+            } else {
+                let save_image = r.image_buffer.into_luma8().save(&path);
+
+                match save_image {
+                    Ok(_) => Ok(format!("Created: {}", &path)),
+                    Err(e) => Err(e.to_string()),
+                }
             }
         }
         Err(e) => Err(format!("Error: {} > {}", e, path)),
